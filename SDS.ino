fa73286f9d1b390c463059adf3ed93d5bb6580af
Aniketh Joshi <46967535+anikethj61@users.noreply.github.com>
2019-04-24 22:09:45 +0530

    Update SDS.ino

--- SDS.ino
+++ SDS.ino
@@ -1,13 +1,15 @@
 void setup() {
   // put your setup code here, to run once:
-
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+Serial.begin(9600)  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(10,LOW);
-  digitalWrite(11,HIGH);
-  delay(10000);
-  digitalWrite(10,HIGH);
-  digitalWrite(11,HIGH);
+digitalWrite(10,LOW);
+digitalWrite(11,HIGH);
+delay(10000);
+digitalWrite(10,HIGH);
+digitalWrite(11,HIGH);
 }
